2301dafc00d6af69169000604a7bf6a5d51d85e2
markup54 <marzenakupka@tlen.pl>
2021-10-12 08:10:17 +0200

    lista produktow

---
+++ app/src/main/java/com/example/listazakupow/ListaProduktow.java
@@ -0,0 +1,25 @@
+package com.example.listazakupow;
+
+import java.util.LinkedList;
+
+public class ListaProduktow {
+    public static LinkedList<String> listaProduktow = new LinkedList<>();
+
+    public ListaProduktow() {
+        listaProduktow.addLast("mleko");
+        listaProduktow.addLast("bułki");
+        listaProduktow.addLast("joghurt");
+        listaProduktow.addLast("jabłka");
+        listaProduktow.addLast("gruszki");
+        listaProduktow.addLast("parówki");
+        listaProduktow.addLast("masło");
+        listaProduktow.addLast("banany");
+        listaProduktow.addLast("ser");
+        listaProduktow.addLast("pieczarki");
+        listaProduktow.addLast("chlebek");
+        listaProduktow.addLast("pietruszka");
+        listaProduktow.addLast("rzodkiewka");
+        listaProduktow.addLast("kawa");
+        listaProduktow.addLast("herbata");
+    }
+}
